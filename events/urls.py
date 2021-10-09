8cf65d9a30bc863a3c89beb6a8457447bb4a04a3
angelptli <ali95@mail.ccsf.edu>
2021-10-08 18:09:41 -0700

    Create a calendar with time page

---
+++ events/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # int: numbers
+    # str: strings
+    # path: whole urls /
+    # slug: hyphen-or_underscores_stuff
+    # UID: universally unique identifier
+    # path('', views.home, name="home"),
+    path('<int:year>/<str:month>/', views.home, name="home"),
+]
---
+++ events/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
+
+def home(request, year, month):
+    name = "Angel"
+    month = month.capitalize()
+
+    # Convert month from name to number
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+
+    # Create a calendar
+    cal = HTMLCalendar().formatmonth(year, month_number)
+
+    # Get current year
+    now = datetime.now()
+    current_year = now.year
+
+    # Get current time
+    time = now.strftime('%I:%M %p')
+
+    return render(request,
+        'home.html', {
+        "name": name,
+        "year": year,
+        "month": month,
+        "month_number": month_number,
+        "cal": cal,
+        "current_year": current_year,
+        "time": time,
+        })
