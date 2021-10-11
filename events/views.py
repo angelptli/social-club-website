d1049579c8af2c18e5fa45e0a21ce394d1f3e0eb
angelptli <ali95@mail.ccsf.edu>
2021-10-10 17:45:22 -0700

    Add update and edit venues feature on website

--- events/views.py
+++ events/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 import calendar
 from calendar import HTMLCalendar
@@ -6,6 +6,22 @@ from datetime import datetime
 from . import models
 from .models import Event, Venue
 from .forms import VenueForm
+
+
+def update_venue(request, venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+    form = VenueForm(request.POST or None, instance=venue)
+
+    if form.is_valid():
+        form.save()
+
+        return redirect('list-venues')
+
+    return render(request,
+        'events/update_venue.html', {
+        "venue": venue,
+        "form":form,
+        })
 
 
 def search_venues(request):
@@ -18,7 +34,8 @@ def search_venues(request):
         return render(request,
             'events/search_venues.html', {
             "searched":searched,
-            "venues":venues})
+            "venues":venues
+            })
 
     else:
         return render(request,
