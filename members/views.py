a65cc77d3de7650006831794b7d1e85714f362e5
angelptli <ali95@mail.ccsf.edu>
2021-10-13 23:56:57 -0700

    Add sign up page with user authentication - phase 2

--- members/views.py
+++ members/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+
 
 def login_user(request):
     if request.method == "POST":
@@ -23,3 +25,21 @@ def logout_user(request):
     messages.success(request, ("You Have Logged Out"))
 
     return redirect('home')
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Sign Up Successful"))
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'authenticate/register_user.html', {
+        "form": form,
+    })
