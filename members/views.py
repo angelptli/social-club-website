439d6af98cafd6a88e550a675610ed5243f90caa
angelptli <ali95@mail.ccsf.edu>
2021-10-13 22:43:46 -0700

    Add user login authentication - phase 1

---
+++ members/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.success(request, ("There Was an Error Logging In, Try Again..."))
+            return redirect('login')
+    else:
+        return render(request, 'authenticate/login.html', {})
